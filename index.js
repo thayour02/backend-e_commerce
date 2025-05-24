7c77f6e49b480999dbe48c8b866d34fb7e5952f1
thayour <temyking@gmail.com>
2025-05-24 22:59:08 +0100

    last

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const User = require('./model/user')
 const app = express()
 
 app.use(cors({
-    origin:"http://localhost:5173", 
+    origin:process.emv.CLIENT_URL , 
     credentials:true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
